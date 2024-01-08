a02debb24eff595fb16f3e407aaf54f925f677fb
Saiarun0007 <138513600+Saiarun0007@users.noreply.github.com>
2024-01-08 12:12:41 +0530

    Update myApp.py 2

--- myApp.py
+++ myApp.py
@@ -4,10 +4,10 @@ st.header("Types of Cats")
 
 col1, col2 = st.columns(2)
 with col1:
-  st.subheader("Persian Pilli")
-  st.image("./persionPilli.jpg", caption="Persian Cat", width=300,use_column_width=True)
-  st.write("I sow it on Little stuart")
+  st.subheader("Son Goku")
+  st.image("./goku.jpg", caption="Son Goku", width=300,use_column_width=True)
+  st.write("kamehameha")
 with col2:
-  st.subheader("Normal Pilli")
-  st.image("./normalPilli.jpeg", caption="Ragdoll Cat", width=300,use_column_width=True)
-  st.write("Just Normal Pilli")
+  st.subheader("Yuta Okkotsu")
+  st.image("./Yuta_Okkotsu.png", caption="Yuta Okkotsu", width=300,use_column_width=True)
+  st.write("GOOD ONE")
